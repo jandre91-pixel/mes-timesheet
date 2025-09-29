af715b888f065ec67729f0df37b9ba97fdb6d863
jandre91-pixel <jandre91@gmail.com>
2025-09-29 17:47:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+});
   plugins: [
     react(),
-    VitePWA({
       registerType: 'autoUpdate',
       manifest: {
         name: 'MES Timesheet',
