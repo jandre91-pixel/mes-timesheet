e86834a37e2decf7cbf864646b25fbaa14cfe6ff
jandre91-pixel <jandre91@gmail.com>
2025-09-29 17:55:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,3 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 });
-  plugins: [
-    react(),
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'MES Timesheet',
-        short_name: 'MES Timesheet',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#111827',
-        icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
-        ]
-      }
-    })
-  ]
-})
